6cfb9498c941c438b411a4172f13b73c4a646e9a
Eran Usha <eusha5@gmail.com>
2018-10-29 13:57:13 -0400

    added JWT to auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://eran:boomer2k@ds023388.mlab.com:23388/connectme"
+  mongoURI: "mongodb://eran:boomer2k@ds023388.mlab.com:23388/connectme",
+  secretOrKey: "secret"
 };
