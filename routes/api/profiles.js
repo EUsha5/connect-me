542539ba316f3daf14059c0443c406288da8e6aa
Eran Usha <eusha5@gmail.com>
2018-10-31 13:32:07 -0400

    Profile validation

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -8,6 +8,9 @@ const Profile = require("../../models/Profile");
 //Load user model
 const User = require("../../models/User");
 
+//Load Validation
+const validateProfileInput = require("../../validation/profile");
+
 //@route GET api/profile/test
 //@desc Tests profile route
 //@access Public
@@ -22,6 +25,7 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -40,6 +44,12 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+    //Check validation
+    if (!isValid) {
+      //Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
     //Get Fields
     const profileFields = {};
     profileFields.user = req.user.id;
@@ -48,7 +58,7 @@ router.post(
     if (req.body.website) profileFields.website = req.body.website;
     if (req.body.location) profileFields.location = req.body.location;
     if (req.body.bio) profileFields.bio = req.body.bio;
-    if (req.body.status) profileFields.handle = req.body.handle;
+    if (req.body.status) profileFields.status = req.body.status;
     if (req.body.githubusername)
       profileFields.githubusername = req.body.githubusername;
     //Skills - split into an Array
