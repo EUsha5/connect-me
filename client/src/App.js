a2e8d8be7ec93e74e51b343400854e36ac78bdb3
Eran Usha <eusha5@gmail.com>
2018-11-07 20:55:21 -0500

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <h1>My React App</h1>
+        <Footer />
       </div>
     );
   }
