1b9d156a50066ef7e458b4bc5ac9abba29e18b80
Eran Usha <eusha5@gmail.com>
2018-11-07 21:36:37 -0500

    added auth links

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 class App extends Component {
   render() {
@@ -14,6 +16,10 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
           <Footer />
         </div>
       </Router>
