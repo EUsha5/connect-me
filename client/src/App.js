eda691e5501abd96e067bc8cfdd30d80d9105be9
Eran Usha <eusha5@gmail.com>
2018-11-15 11:31:11 -0500

    created authReducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-
+import store from "./store";
 import "./App.css";
-
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
-
-const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
