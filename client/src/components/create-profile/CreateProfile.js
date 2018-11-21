0b55e97766a1d0d3189e0d5d011eef7a7df8738c
Eran Usha <eusha5@gmail.com>
2018-11-21 14:09:34 -0500

    added SelectList to profile creation

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import SelectListGroup from "../common/SelectListGroup";
+import InputGroup from "../common/InputGroup";
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -23,8 +26,32 @@ class CreateProfile extends Component {
       youtube: "",
       errors: {}
     };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
+  onSubmit(e) {
+    e.preventDefault();
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
+    const { errors } = this.state;
+
+    //Select options for status
+    const options = [
+      { label: "*Select Professional Status", value: 0 },
+      { label: "Junior Developer", value: "Junior Developer" },
+      { label: "Senior Developer", value: "Senior Developer" },
+      { label: "Manager", value: "Manager" },
+      { label: "Student or Learning", value: "Student or Learning" },
+      { label: "Instructor or Teacher", value: "Instructor or Teacher" },
+      { label: "Intern", value: "Intern" },
+      { label: "Other", value: "Other" }
+    ];
+
     return (
       <div className="create-profile">
         <div className="container">
@@ -35,6 +62,25 @@ class CreateProfile extends Component {
                 Let's get some information to make your profile standout
               </p>
               <small className="d-block pb-3">* = required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your URL. Your full name, company name, nickname"
+                />
+                <SelectListGroup
+                  placeholder="* Status"
+                  name="status"
+                  value={this.state.status}
+                  onChange={this.onChange}
+                  options={options}
+                  error={errors.status}
+                  info="Give us an idea of where you are at in your career"
+                />
+              </form>
             </div>
           </div>
         </div>
