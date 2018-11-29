e36c3e6149480913bbae454b55d5849f7d895450
Eran Usha <eusha5@gmail.com>
2018-11-29 17:09:31 -0500

    github user info and not-found

---
+++ client/src/components/not-found/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">Page Does Not Exist</h1>
+      <p>Sorry, this page does not exist</p>
+    </div>
+  );
+};
